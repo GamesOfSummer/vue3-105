0f863fb741e2ff8ee5fcd7f946c71be53549e485
Summer T <summer@summertime-development.com>
2022-03-02 08:33:53 -0600

    fixing errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,21 @@
+const { off } = require("process");
+
 /* eslint-env node */
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   "root": true,
+
+
+  parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
   "extends": [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
@@ -11,5 +24,9 @@ module.exports = {
   ],
   "env": {
     "vue/setup-compiler-macros": true
+  },
+  "rules":
+  {
+    "importsNotUsedAsValues" : off,
   }
 }
--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,24 +1,21 @@
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-import Task from "@/models/Task";
-import { useStore } from "@/store";
-import { MutationType } from "@/store/mutations";
-import { useRouter } from "vue-router";
+import { defineComponent, ref } from 'vue';
+import Task from '../model/Task';
+import { useStore } from '@/store';
+import { MutationType } from '@/store/mutations';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
-  name: "AddTask",
-  setup() {
-    const taskName = ref("");
-    const router = useRouter(); // Substitute of Vue prototype this.$router
-    const store = useStore();
-    const addTask = (): void => {
-      const newTask = new Task(taskName.value);
-      store.commit(MutationType.SetTask, newTask);
-      taskName.value = "";
-      router.push({ path: "/" });
-    };
+    name: 'AddTask',
+    setup() {
+        const taskName = ref('');
+        const router = useRouter(); // Substitute of Vue prototype this.$router
+        const store = useStore();
+        const addTask = (): void => {
+            store.commit(MutationType.SetTask, { value: '' });
+        };
 
-    return { taskName, addTask };
-  },
+        return { taskName, addTask };
+    },
 });
 </script>
